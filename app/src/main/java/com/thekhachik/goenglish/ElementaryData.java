f97a40c27df6bcbce6c6db5fd7d67d10cd0eec32
Eugene <maskot2002@yandex.ru>
2022-04-27 20:48:59 +0300

    Update 2.1 Add test

--- app/src/main/java/com/thekhachik/goenglish/ElementaryData.java
+++ app/src/main/java/com/thekhachik/goenglish/ElementaryData.java
@@ -14,11 +14,13 @@ public class ElementaryData {
             "Olivia __________ to Rome.", "Ive met Robbie Williams. When __________ ?", "Raquel and James have _______ had a baby.", "Has Nigel cooked dinner _____ ?",
             "________ 's the weather like in Mexico City?", "My grandmother started work ________1960.", "It's snowy — please drive _____ .", "Let's go for a ______ swim."));
 
-    static ArrayList<String> elemA = new ArrayList<>(Arrays.asList("didn’t went", " this morning", " did/play", "do/any", "many", "some", "older than", "the most beautiful", "the better", "wear", "Do / work", "going to rain", "does / going to stay", "seeing", "When", "£8.50", "Have / eat", "has never been", "have you met", "yet", "yet", "low", "in", "carefully", "quick"));
+    static ArrayList<String> elemA = new ArrayList<>(Arrays.asList("didn’t went", " this morning", "did/play", "do/any", "many", "some", "older than", "the most beautiful", "the better", "wear", "Do / work", "going to rain", "does / going to stay", "seeing", "When", "£8.50", "Have / eat", "has never been", "have you met", "yet", "yet", "low", "in", "carefully", "quick"));
     static ArrayList<String> elemB = new ArrayList<>(Arrays.asList("don’t go", "three hours", "do/play", "are/some", "much", "any", "more older than", "the beautiful lest", "the good", "wearing", "Is / working", "rainy", "— / is going to stay", "to see", "Why", "Hewitt's Bookshop", "Have / ate", "has ever been", "did you met him", "just", "ever", "What", "by", "easily", "badly"));
     static ArrayList<String> elemC = new ArrayList<>(Arrays.asList("didn’t go", "7.30 a.m.", " did/played", "would/some", "a", "the", "much old than", "more beautiful than", "the best", "wearing", "Does / works", "raining", "is / going to stay", "going to see", "Where", "To read some Shakespeare", "Have / eaten", "hasn't never been", "did you meet him", "ever", "just", "Which", "on", "careful", "well"));
 
     static ArrayList<Integer> answers = new ArrayList<>(Arrays.asList(3, 2, 1, 3, 2, 2, 1, 1, 3, 3, 2, 1, 3, 2, 3, 1, 3, 1, 3, 2, 1, 2, 1, 1, 1));
+
+    static ArrayList<String> answers2 = new ArrayList<>(Arrays.asList("didn’t go", "three hours", "did/play", "would/some", "much", "any", "older than", "the most beautiful", "the best", "wearing", "Is / working", "going to rain", "is / going to stay", "to see", "Where", "£8.50", "Have / eaten", "has never been", "did you meet him", "just", "yet", "What", "in", "carefully", "quick"));
 
     static int scoreElem;
 }
--- app/src/main/java/com/thekhachik/goenglish/TestStartFragment.java
+++ app/src/main/java/com/thekhachik/goenglish/TestStartFragment.java
@@ -3,6 +3,8 @@ package com.thekhachik.goenglish;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,12 +13,13 @@ import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class TestStartFragment extends Fragment {
 
-    public TextView textTask;
+    public TextView textTask, ttt;
     public RadioButton answer1, answer2, answer3;
     public RadioGroup groupBtn;
     public Button next;
@@ -25,10 +28,12 @@ public class TestStartFragment extends Fragment {
     public ArrayList<String> ListElemA;
     public ArrayList<String> ListElemB;
     public ArrayList<String> ListElemC;
-    public ArrayList<Integer> ListAnswer;
+    public ArrayList<String> ListAnswer;
 
-    public ArrayList<Boolean> TrueOrFalseAnswer;
+    public ArrayList<Boolean> TrueOrFalseAnswer = new ArrayList<>();
+    public Boolean bool;
 
+    //Счетчик для списков
     public int ig = 0;
 
     @Override
@@ -42,6 +47,7 @@ public class TestStartFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_test_start, container, false);
 
+        //Связываем все элементы из XML
         textTask = view.findViewById(R.id.textTask);
         answer1 = view.findViewById(R.id.answer1);
         answer2 = view.findViewById(R.id.answer2);
@@ -49,32 +55,91 @@ public class TestStartFragment extends Fragment {
         groupBtn = view.findViewById(R.id.groupBtn);
         next = view.findViewById(R.id.button);
 
+        //Если был выбран тест Elementary, то мы передаем значения списков из ElementaryData в этот файл.
         if(Data.checkTest == 1){
             ListTask = ElementaryData.elementaryListTest;
             ListElemA = ElementaryData.elemA;
             ListElemB = ElementaryData.elemB;
             ListElemC = ElementaryData.elemC;
-            ListAnswer = ElementaryData.answers;
+            ListAnswer = ElementaryData.answers2;
         }
 
-
+        //Добавляем текст с заданием и пункты ответов.
         textTask.setText(ListTask.get(ig));
         answer1.setText(ListElemA.get(ig));
         answer2.setText(ListElemB.get(ig));
         answer3.setText(ListElemC.get(ig));
 
+        //Проверка какой ответ верный
+        groupBtn.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i){
+                    case R.id.answer1:
+                        if(answer1.getText().toString().equals(ListAnswer.get(ig))){
+                            bool = true;
+                        }
+                        else{
+                            bool = false;
+                        }
+                        break;
 
+                    case R.id.answer2:
+                        if(answer2.getText().toString().equals(ListAnswer.get(ig))){
+                            bool = true;
+                        }
+                        else{
+                            bool = false;
+                        }
+                        break;
+
+                    case R.id.answer3:
+                        if(answer3.getText().toString().equals(ListAnswer.get(ig))){
+                            bool = true;
+                        }
+                        else{
+                            bool = false;
+                        }
+                        break;
+                }
+            }
+        });
+
+        //Следующее задание.
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ig++;
-                groupBtn.clearCheck();
-                textTask.setText(ListTask.get(ig));
-                answer1.setText(ListElemA.get(ig));
-                answer2.setText(ListElemB.get(ig));
-                answer3.setText(ListElemC.get(ig));
-
-
+                if(answer1.isChecked() || answer2.isChecked() || answer3.isChecked()){
+                    if(ig+1 <= ListTask.size()-1){
+                        //Увеличиваем счетчик на 1, чтобы указать следуюзие задания
+                        ig++;
+                        //Добавляем в список наши ответы, для результата.
+                        TrueOrFalseAnswer.add(bool);
+                        //Убираем выделение с RadioButton.
+                        groupBtn.clearCheck();
+                        //После добавления результата, обнуляем значение для того чтобы в следующем тесте эта переменная была пустая
+                        bool = null;
+                        //Установить следующее задание
+                        textTask.setText(ListTask.get(ig));
+                        answer1.setText(ListElemA.get(ig));
+                        answer2.setText(ListElemB.get(ig));
+                        answer3.setText(ListElemC.get(ig));
+                    }
+                    else {
+                        Fragment fragment = new TestsFragment();
+                        FragmentManager fm = getActivity().getSupportFragmentManager();
+                        FragmentTransaction ft = fm.beginTransaction();
+                        ft.setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out);
+                        ft.replace(R.id.mainFrame, fragment);
+                        ft.commit();
+                    }
+                }
+                else {
+                    Toast toast = Toast.makeText(getContext(), "Выберите ответ", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                System.out.println(ig);
+                System.out.println(TrueOrFalseAnswer);
             }
         });
 
@@ -82,11 +147,6 @@ public class TestStartFragment extends Fragment {
     }
 
     public void checkTask(){
-        groupBtn.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
 
-            }
-        });
     }
 }
